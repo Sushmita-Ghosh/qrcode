da949e0207259c2a93b6192ab7f781bdf143a5e9
Sushmita Ghosh <pujaghosh251096@gmail.com>
2024-03-31 00:39:59 -0700

    [Outline]

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import QRcodeComponent from "./components/QRcodeComponent";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-light-gray">
+      <QRcodeComponent />
+    </div>
+  );
+}
+
+export default App;
